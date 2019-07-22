2492db1fe1c43c40e11ef0306350dbfafa6ccfb1
Seil Cho <seilcho7@hotmail.com>
2019-07-22 19:45:11 -0400

    added paramMap

--- src/app/department-detail/department-detail.component.ts
+++ src/app/department-detail/department-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-department-detail',
@@ -9,10 +9,27 @@ import { ActivatedRoute } from '@angular/router';
 export class DepartmentDetailComponent implements OnInit {
 
   public departmentId;
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
-    let id = parseInt(this.route.snapshot.paramMap.get('id'));
-    this.departmentId = id;
+    // let id = parseInt(this.route.snapshot.paramMap.get('id'));
+    // this.departmentId = id;
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      let id = parseInt(params.get('id'));
+      this.departmentId = id;
+    });
+  }
+
+  goPrevious() {
+    if (this.departmentId > 0) {
+      let previousId = this.departmentId - 1;
+      this.router.navigate(['/departments', previousId]);
+    }
+  }
+
+  goNext() {
+    let nextId = this.departmentId + 1;
+    this.router.navigate(['/departments', nextId]);
+
   }
 }
