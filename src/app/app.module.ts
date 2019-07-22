5c52ec22c197cbe605ed48ac76348a47c783674c
Seil Cho <seilcho7@hotmail.com>
2019-07-22 14:35:58 -0400

    adds components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { DepartmentListComponent } from './department-list/department-list.component';
+// import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // DepartmentListComponent,
+    // EmployeeListComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
